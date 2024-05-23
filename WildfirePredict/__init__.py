15f1abfe07e8d60c747004ba8ec97828566bc7b8
edsml-rg623 <ruida.guo23@imperial.ac.uk>
2024-05-23 12:14:37 +0100

    delete predict module

--- WildfirePredict/__init__.py
+++ WildfirePredict/__init__.py
@@ -1,7 +1,6 @@
 from .dataset.dataset import WildfireDataset
 from .model.model import ConvLSTMModel
 from .training.training import WildfireTrainer
-from .predict.predict import WildfirePredictor
 from .utils.split_dataset import *
 from .utils.plot_sequences import *
 from .utils.predict_image import *
