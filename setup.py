1e90d684f071599a5b580d741a9fc38c22480ea4
edsml-jv323 <jorge.veiras-yanes23@imperial.ac.uk>
2024-05-24 11:25:20 +0100

    Setup flakes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'requirements.txt'), 'r', encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
-# define setup 
+# define setup
 setup(
     name='WildfireThomas',
     version='0.1.0',
@@ -15,7 +15,8 @@ setup(
     install_requires=requirements,
     author='IMPERIAL COLLEGE LONDON MSC EDSML - GROPU THOMAS',
     description='A package for wildfire prediction and data assimilation',
-    long_description=open(os.path.join(here, 'README.md'), encoding="utf-8").read(),
+    long_description=open(os.path.join(here, 'README.md'),
+                          encoding="utf-8").read(),
     long_description_content_type='text/markdown',
     url='https://github.com/ese-msc-2023/acds3-wildfire-thomas',
     classifiers=[
